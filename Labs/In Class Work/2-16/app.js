7a3e01f5c64a2a4f730dc9a059085b333af56550
Jake-Muilen <jakemuilenburg1234@gmail.com>
2023-02-16 12:35:55 -0500

    lab

---
+++ Labs/In Class Work/2-16/app.js
@@ -0,0 +1,33 @@
+
+/*
+for (let i = 0; i < 10; i++) {
+    console.log(i+1);
+  }
+*/
+
+/*
+let words = [ "Indiana", "University", "Purdue", "University", "Indianapolis" ]
+
+for (let i = 0; i<5; i++){
+    console.log(words[i]+"! Yeah!");
+}
+*/
+
+/*
+let words = [ "Indiana", "University", "Purdue", "University", "Indianapolis" ]
+
+for (let i = 0; i<5; i++){
+
+    console.log(words[i].toUpperCase());
+}
+*/
+
+/*
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+  }
+*/
+
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+  }
