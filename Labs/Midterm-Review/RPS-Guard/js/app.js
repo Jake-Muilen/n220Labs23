67719b61919ae0263bf45516d01209c684d11418
Jake-Muilen <jakemuilenburg1234@gmail.com>
2023-03-02 01:46:12 -0500

    Algorithm

---
+++ Labs/Midterm-Review/RPS-Guard/js/app.js
@@ -0,0 +1,11 @@
+//Jake Muilenburg
+//N220
+//3/2/2023
+
+//Make 4 Divs with each move option
+//Make a global variable for score started at 0
+//create a random move generator for computer that activates when any of the divs are clicked
+//create a set of if statements that check to see if computor won or lost
+//if player chooses guard subtract 1/2 a point from score
+// display the result of the round, ie what player chose and what computer chose
+//update score
---
+++ Labs/Midterm-Review/UI-Nav/js/app.js
@@ -0,0 +1,8 @@
+//Jake Muilenburg
+//N220
+//3/2/2023
+
+//Make 6 divs
+//Make a selected div
+//make an If Clicked statement that changes the text to bold and changes the divs background
+// display the clicked divs tex in the selected div
